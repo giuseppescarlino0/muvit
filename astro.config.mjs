b67c5283bd5b37c2eb47a13b1b4d17739d0b8c22
Giuseppe Scarlino <s_giuseppe90@hotmail.it>
2026-03-11 21:57:50 +0100

    fix sitemap customPages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: "https://muvit.it", // IMPORTANT: cambia quando hai il dominio finale
-  integrations: [sitemap()],
+  site: 'https://muvit.it',
+  integrations: [
+    sitemap({
+      customPages: [
+        '/contatti',
+        '/flotta',
+        '/perche-muvit',
+        '/qualita'
+      ]
+    })
+  ]
 });
