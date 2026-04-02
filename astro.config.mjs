00a4c2e47a811cd75ba679d93abea71ae3743a54
Giuseppe Scarlino <s_giuseppe90@hotmail.it>
2026-04-02 17:31:47 +0200

    fix mobile contact page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://muvit.it',
+  site: 'https://muvit.cloud',
   integrations: [
     sitemap({
       customPages: [
